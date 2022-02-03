1afb64d6683dfbacb397ea7be946ca528ed10190
Hannah Law <hannah.law@portswigger.net>
2022-02-03 13:21:48 +0000

    putClientProperty on Swing components that utilise HTML rendering set to null

--- src/burp/Utils/DiffViewerPane.java
+++ src/burp/Utils/DiffViewerPane.java
@@ -7,11 +7,13 @@ public class DiffViewerPane extends JEditorPane {
   public DiffViewerPane(byte[] original, byte[] modified) {
     this.setEditable(false);
     this.setContentType("text/html");
+    this.putClientProperty("html.disable", null);
   }
 
   public DiffViewerPane() {
     this.setEditable(false);
     this.setContentType("text/html");
+    this.putClientProperty("html.disable", null);
   }
 
 
